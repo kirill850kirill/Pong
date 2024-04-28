f6be8711dd0994756362a52bab42409d551258a4
kirill850kirill <167691412+kirill850kirill@users.noreply.github.com>
2024-04-28 16:53:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,29 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+
+racket1 = Player('racket.png', 30, 200,4,50,150)
+racket2 = Player('racket.png', 520, 200,4,50,150)
 ball = GameSprite('ball.png', 225,225,4,50,50)
 
+
+speed_x = 3
+speed_y = 3
 
 
 window = display.set_mode((500, 500))
@@ -27,6 +48,19 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+    
 
     if not finish:
         ball.reset()
