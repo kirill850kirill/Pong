8efb660fc2561fdca2291d118b39b80155f6b634
kirill850kirill <167691412+kirill850kirill@users.noreply.github.com>
2024-04-28 17:05:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from pygame import*
+from pygame import *
+
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (wight, height))  # вместе 55,55 - параметры
+        self.image = transform.scale(image.load(player_image), (wight, height)) 
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -29,25 +30,36 @@ class Player(GameSprite):
             self.rect.y += self.speed
 
 
-racket1 = Player('racket.png', 30, 200,4,50,150)
-racket2 = Player('racket.png', 520, 200,4,50,150)
-ball = GameSprite('ball.png', 225,225,4,50,50)
 
+back = (200, 255, 255)  
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
 speed_x = 3
 speed_y = 3
 
-
-window = display.set_mode((500, 500))
-col1 = (0,0, 255)
-mw.fill(col1)
-cloock = time.Clock()
-
-run = True
-while run:
+while game:
     for e in event.get():
         if e.type == QUIT:
-            run = False
+            game = False
 
     if finish != True:
         window.fill(back)
@@ -60,9 +72,25 @@ while run:
             speed_x *= -1
             speed_y *= 1
 
-    
+        
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1
 
-    if not finish:
+        
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+
+        
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+
+        racket1.reset()
+        racket2.reset()
         ball.reset()
+
     display.update()
-    cloock/tick(40)
+    clock.tick(FPS)
